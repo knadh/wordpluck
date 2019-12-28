560eb214b2d9886d8dc66139c7545ef8eaffaca9
Vinoth Kumar V <vinoth.kumar@zerodha.com>
2019-12-28 21:39:57 +0530

    variables moved fomr global scope to class properties

--- js/main.js
+++ js/main.js
@@ -5,32 +5,31 @@
 	November 2012
 */
 
-	// globals
-	var prompts = {
-		2: "Easy peasy!",
-		4: "Not bad, ey?",
-		5: "Make it rain!",
-		7: "Careful now!",
-		8: "Oh oh ...",
-		10: "Whew!",
-		12: "Make it rain!",
-		15: "Watch the speed!",
-		18: "You're still here!",
-		20: "That's insane :O",
-		30: "Don't stop now!",
-		40: "You are the ONE",
-		50: "Umm ... level 50",
-		60: "You're still here!"
-	};
-
-	var	RADIUS = 30,
-		BORDER = 4,
-		FONT_SIZE = 36,
-		FPS = 60,
-		SCORE_LETTER = 10,
-		WORDS_PER_LEVEL = 3,
-
-		round = {
+class Game {
+	constructor() {
+		this.prompts = {
+			2: "Easy peasy!",
+			4: "Not bad, ey?",
+			5: "Make it rain!",
+			7: "Careful now!",
+			8: "Oh oh ...",
+			10: "Whew!",
+			12: "Make it rain!",
+			15: "Watch the speed!",
+			18: "You're still here!",
+			20: "That's insane :O",
+			30: "Don't stop now!",
+			40: "You are the ONE",
+			50: "Umm ... level 50",
+			60: "You're still here!"
+		}
+		this.RADIUS = 30
+		this.BORDER = 4
+		this.FONT_SIZE = 36
+		this.FPS = 60
+		this.SCORE_LETTER = 10
+		this.WORDS_PER_LEVEL = 3
+		this.round = {
 			"elapsed": 0,
 			"word_count": 0,
 			"pop_count": 0,
@@ -41,24 +40,20 @@
 			"speed": 0
 		}
 
-		words = ["hello"],
-		bubbles = {},
+		this.words = ["hello"]
+		this.bubbles = {}
+		this.canvas = null
+		this.stage = null
+		this.bounds = null
+		this.sounds = [],
+		this.cur = null
+		this.start_time = null
+		this.started = false
+		this.sound = true
+		this.tips = false
+		this.ui = {}
+		this.notice_timer = null
 
-		canvas = null,
-		stage = null,
-		bounds = null,
-		sounds = [],
-		cur = null,
-		start_time = null,
-		started = false,
-
-		sound = true,
-		tips = false,
-		ui = {},
-		notice_timer = null;
-
-class Game {
-	constructor() {
 		this.init = function() {
 			return this.initialize();
 		}
@@ -69,7 +64,7 @@ class Game {
 		this.initUI();
 
 		// load words
-		this.loadWords(round.level);
+		this.loadWords(this.round.level);
 		this.nextWord();
 
 		// update ui elements
@@ -78,30 +73,30 @@ class Game {
 
 	// play/pause the game
 	toggleGame() {
-		if(!started) return false;
+		if(!this.started) return false;
 
 		if(createjs.Ticker.getPaused()) {
 			this.clearNotice();
-			canvas.style.opacity = 1;
+			this.canvas.style.opacity = 1;
 			createjs.Ticker.setPaused(false);
 		} else {
 			createjs.Ticker.setPaused(true);
-			canvas.style.opacity = 0;
+			this.canvas.style.opacity = 0;
 			this.permaNotice("Hit Space to unpause");
 		}
 	}
 
 	// init ui elements
 	initUI() {
-		canvas = document.querySelector("#stage");
+		this.canvas = document.querySelector("#stage");
 
 		// canvas bounds
-		bounds = new createjs.Rectangle();
+		this.bounds = new createjs.Rectangle();
 
 		// set the stage
-		stage = new createjs.Stage(canvas);
+		this.stage = new createjs.Stage(this.canvas);
 
-		ui = {
+		this.ui = {
 			"controls": document.querySelector("#controls"),
 			"score": document.querySelector("#score"),
 			"notice": document.querySelector("#notice"),
@@ -117,12 +112,12 @@ class Game {
 
 		// start
 		document.querySelector("#btn-start").addEventListener('click', ()=>{
-			ui.start_box.remove();
+			this.ui.start_box.remove();
 
 			this.notice("Prepare to start typing ...");
 			window.setTimeout(() => {
-				started = true;
-				createjs.Ticker.setFPS(FPS);
+				this.started = true;
+				createjs.Ticker.setFPS(this.FPS);
 				// createjs.Ticker.addListener(this.tick, true);
 				createjs.Ticker.addListener(this.tick.bind(this), true)
 
@@ -133,37 +128,37 @@ class Game {
 		});
 
 		// sound checkbox
-		ui.sound.onclick = function() {
+		this.ui.sound.onclick = function() {
 			if(this.checked) {
-				sound = true;
+				this.sound = true;
 			} else {
-				sound = false;
+				this.sound = false;
 			}
-			localStorage.sound = sound;
+			localStorage.sound = this.sound;
 		};
 		if( typeof localStorage.sound == "undefined" || localStorage.sound == "true") {
-			ui.sound.checked = true; sound = true;
+			this.ui.sound.checked = true; this.sound = true;
 		} else {
-			ui.sound.checked = false; sound = false;
+			this.ui.sound.checked = false; this.sound = false;
 		}
 
 		// tips checkbox
-		ui.tips.onchange = function() {
-			ui.tip.innerText = "";
+		this.ui.tips.onchange = function() {
+			this.ui.tip.innerText = "";
 			if(this.checked) {
-				tips = true;
+				this.tips = true;
 			} else {
-				tips = false;
+				this.tips = false;
 			}
-			localStorage.tips = tips;
+			localStorage.tips = this.tips;
 		};
 		if( typeof localStorage.tips == "undefined" || localStorage.tips == "false") {
-			ui.tips.checked = false; tips = false;
+			this.ui.tips.checked = false; this.tips = false;
 		} else {
-			ui.tips.checked = true; tips = true;
+			this.ui.tips.checked = true; this.tips = true;
 		}
 
-		canvas.addEventListener('click', () => {
+		this.canvas.addEventListener('click', () => {
 			this.toggleGame();
 		})
 
@@ -171,9 +166,9 @@ class Game {
 		for(var n=0; n<15; n++) {
 			var snd = new Audio();
 				snd.src = "sounds/pluck." + ( snd.canPlayType("audio/mpeg") ? "mp3" : "ogg" );
-			sounds.push(snd);
+			this.sounds.push(snd);
 		}
-		sounds.p = 1;
+		this.sounds.p = 1;
 
 		this.positionUI();
 		window.onresize = this.positionUI;
@@ -181,26 +176,26 @@ class Game {
 
 	// position ui elements
 	positionUI() {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
 
-		bounds.width = canvas.width;
-		bounds.height = canvas.height;
+		this.bounds.width = this.canvas.width;
+		this.bounds.height = this.canvas.height;
 
-		RADIUS = Math.ceil(canvas.width * 0.019);
-		FONT_SIZE = Math.ceil(canvas.width * 0.022);
-		BORDER = RADIUS * .13;
+		this.RADIUS = Math.ceil(this.canvas.width * 0.019);
+		this.FONT_SIZE = Math.ceil(this.canvas.width * 0.022);
+		this.BORDER = this.RADIUS * .13;
 	}
 
 	// show a permanent notice
 	permaNotice(msg) {
-		window.clearTimeout(notice_timer);
-		ui.notice.innerText = msg;
-		ui.notice.style.display = "block";
+		window.clearTimeout(this.notice_timer);
+		this.ui.notice.innerText = msg;
+		this.ui.notice.style.display = "block";
 	}
 	// hide notice
 	clearNotice(msg) {
-		ui.notice.style.display = "none";
+		this.ui.notice.style.display = "none";
 	}
 
 	// show a delayed
@@ -212,49 +207,49 @@ class Game {
 
 	// show a notice
 	notice(msg) {
-		window.clearTimeout(notice_timer);
+		window.clearTimeout(this.notice_timer);
 
-		ui.notice.innerText = msg;
-		ui.notice.style.display = "block";
+		this.ui.notice.innerText = msg;
+		this.ui.notice.style.display = "block";
 
-		notice_timer = window.setTimeout(function() {
-			ui.notice.style.display = "none";
+		this.notice_timer = window.setTimeout(() => {
+			this.ui.notice.style.display = "none";
 		}, 3000)
 	}
 
 	// play a sound
 	playSound(s) {
-		if(!sound) return;
+		if(!this.sound) return;
 
-		sounds[sounds.p-1].pause();
-		sounds[sounds.p-1].load();
-		sounds[sounds.p].play();
+		this.sounds[this.sounds.p-1].pause();
+		this.sounds[this.sounds.p-1].load();
+		this.sounds[this.sounds.p].play();
 
-		if(sounds.p >= sounds.length-1) {
-			sounds.p = 1;
+		if(this.sounds.p >= this.sounds.length-1) {
+			this.sounds.p = 1;
 		} else {
-			sounds.p++;
+			this.sounds.p++;
 		}
 	}
 
 	// update ui elements
 	updateUI() {
-		ui.level.innerText = round.level;
-		ui.score.innerText = round.score;
-		ui.words.innerText = round.pop_count;
-		ui.accuracy.innerText =  ((round.accuracy / round.word_count) * 100).toFixed(1) + "%";
+		this.ui.level.innerText = this.round.level;
+		this.ui.score.innerText = this.round.score;
+		this.ui.words.innerText = this.round.pop_count;
+		this.ui.accuracy.innerText =  ((this.round.accuracy / this.round.word_count) * 100).toFixed(1) + "%";
 
-		var wpm = (( Math.round((round.letter_count/5), 0) / round.elapsed) * 60).toFixed(2);
-		round.speed = wpm*1 == wpm ? wpm : 0;
-		ui.speed.innerText = round.speed;
+		var wpm = (( Math.round((this.round.letter_count/5), 0) / this.round.elapsed) * 60).toFixed(2);
+		this.round.speed = wpm*1 == wpm ? wpm : 0;
+		this.ui.speed.innerText = this.round.speed;
 	}
 
 	// level goes up
 	levelUp() {
-		round.level++;
-		this.notice("Level " + round.level);
+		this.round.level++;
+		this.notice("Level " + this.round.level);
 		this.updateUI();
-		this.loadWords(round.level);
+		this.loadWords(this.round.level);
 	}
 
 	// create a word and render it
@@ -264,16 +259,16 @@ class Game {
 		var g = new createjs.Graphics();
 
 		// draw the circle
-		g.setStrokeStyle(BORDER);
+		g.setStrokeStyle(this.BORDER);
 		g.beginStroke("#333333");
-		g.drawCircle(0,0, RADIUS);
+		g.drawCircle(0,0, this.RADIUS);
 
 
 		var x = 100, set = [];
 		for(var n=0; n<word.length; n++) {
 			var bubble = new createjs.Container(),
 				circle = new createjs.Shape(g),
-				letter = new createjs.Text(word[n] == " " ? "_" : word[n], FONT_SIZE + "px Arial", "#333333");
+				letter = new createjs.Text(word[n] == " " ? "_" : word[n], this.FONT_SIZE + "px Arial", "#333333");
 
 			letter.textBaseline = "middle";
 			letter.textAlign = "center";
@@ -283,28 +278,28 @@ class Game {
 			bubble.char = word[n];
 			bubble.temp_x = x;
 			bubble.y = 10;
-			bubble.speed = this.random((round.level / 5) + 1, (round.level / 5) + 3); // initial speed
+			bubble.speed = this.random((this.round.level / 5) + 1, (this.round.level / 5) + 3); // initial speed
 
 			set.push(bubble);
-			stage.addChild(bubble);
-			x+= RADIUS + this.random( RADIUS+10, RADIUS*2 + 20);
+			this.stage.addChild(bubble);
+			x+= this.RADIUS + this.random( this.RADIUS+10, this.RADIUS*2 + 20);
 		}
 
 		for(var n=0; n<set.length; n++) {
 			var bubble = set[n];
-			bubble.x = bubble.temp_x + ( (canvas.width - x) / 2);
+			bubble.x = bubble.temp_x + ( (this.canvas.width - x) / 2);
 		}
 
 		set.pointer = 0; // character pointer
 		set.deleted = 0; // deleted chars
 		set.bad = 0; // bad keypresses
 
-		bubbles[word] = set;
-		cur = word;
-		round.word_count++;
+		this.bubbles[word] = set;
+		this.cur = word;
+		this.round.word_count++;
 
 		// level goes up
-		if(round.word_count % WORDS_PER_LEVEL == 0) {
+		if(this.round.word_count % this.WORDS_PER_LEVEL == 0) {
 			this.levelUp();
 		}
 
@@ -313,93 +308,93 @@ class Game {
 
 	// render the upcoming word
 	renderTip(word) {
-		if(!tips || round.word_count == 1) return;
-		ui.tip.innerText = word;
+		if(!this.tips || this.round.word_count == 1) return;
+		this.ui.tip.innerText = word;
 	}
 
 	// createjs ticker
 	tick() {
-		for(var word in bubbles) {
-			if(!bubbles.hasOwnProperty(word)) continue;
+		for(var word in this.bubbles) {
+			if(!this.bubbles.hasOwnProperty(word)) continue;
 
 			// go through all bubbles in the queue and animate them
-			for(var n=0; n<bubbles[word].length; n++) {
-				if(!bubbles[word][n]) continue;
+			for(var n=0; n<this.bubbles[word].length; n++) {
+				if(!this.bubbles[word][n]) continue;
 
-				var bubble = bubbles[word][n];
-				if(bubble.y - RADIUS > bounds.height) { // if a bubble's crossed the Y boundary, kill it
-					bubbles[word].deleted++;
-					stage.removeChild(bubbles[word][n]);
-					delete bubbles[word][n];
+				var bubble = this.bubbles[word][n];
+				if(bubble.y - this.RADIUS > this.bounds.height) { // if a bubble's crossed the Y boundary, kill it
+					this.bubbles[word].deleted++;
+					this.stage.removeChild(this.bubbles[word][n]);
+					delete this.bubbles[word][n];
 				} else {
 					bubble.y += bubble.speed;
 				}
 			}
 
 			// wipe out a fallen word completely
-			if(bubbles[word].deleted == bubbles[word].length) {
-				var popped = bubbles[cur].pointer >= bubbles[cur].length;
-				delete bubbles[word];
+			if(this.bubbles[word].deleted == this.bubbles[word].length) {
+				var popped = this.bubbles[this.cur].pointer >= this.bubbles[this.cur].length;
+				delete this.bubbles[word];
 				this.deleted(popped);
 			}
 		}
 
-		stage.update();
+		this.stage.update();
 	};
 
 	// load words from the dictionary
 	loadWords(level) {
 		if(level == 1) {
-			words = THESAURUS.three;
+			this.words = THESAURUS.three;
 		} else if(level == 3) {
-			words = THESAURUS.small;
+			this.words = THESAURUS.small;
 		} else if(level == 5) {
-			words = THESAURUS.medium;
+			this.words = THESAURUS.medium;
 		} else if(level == 8) {
-			words = THESAURUS.large;
+			this.words = THESAURUS.large;
 		} else if(level == 10) {
-			words = THESAURUS.big;
+			this.words = THESAURUS.big;
 		} else if(level == 14) {
-			words = THESAURUS.medium;
-		} else if(words.length < 2) {
-			words = THESAURUS.medium;
+			this.words = THESAURUS.medium;
+		} else if(this.words.length < 2) {
+			this.words = THESAURUS.medium;
 		}
 
-		if(prompts.hasOwnProperty(level)) {
-			this.delayedNotice(prompts[level], 5);
+		if(this.prompts.hasOwnProperty(level)) {
+			this.delayedNotice(this.prompts[level], 5);
 		}
 
-		words = this.shuffle(words);
-		words.index = 0;
+		this.words = this.shuffle(this.words);
+		this.words.index = 0;
 	}
 
 	// up the next word in the queue
 	nextWord() {
-		this.renderWord(words[words.index]);
-		words.index = words.index+1 >= words.length ? 0 : words.index+1;
-		this.renderTip(words[words.index]);
+		this.renderWord(this.words[this.words.index]);
+		this.words.index = this.words.index+1 >= this.words.length ? 0 : this.words.index+1;
+		this.renderTip(this.words[this.words.index]);
 	}
 
 	// starttime of a word's creation
 	markStart() {
-		start_time = this.microtime();
+		this.start_time = this.microtime();
 	}
 
 	// note elapsed time
 	elapsed() {
-		round.elapsed += (this.microtime() - start_time);
+		this.round.elapsed += (this.microtime() - this.start_time);
 	}
 
 	// successful pop of a word
 	score(word) {
 		// successful pop of a word
-		var score = word.length * (tips ? SCORE_LETTER/2 : SCORE_LETTER);
-		round.letter_count+= word.length;
-		round.pop_count++;
+		var score = word.length * (this.tips ? this.SCORE_LETTER/2 : this.SCORE_LETTER);
+		this.round.letter_count+= word.length;
+		this.round.pop_count++;
 
 		this.elapsed();
 
-		round.score += score;
+		this.round.score += score;
 	};
 
 	// a single character's been popped
@@ -410,20 +405,20 @@ class Game {
 	// a word's been successfully popped by the user
 	popped(word) {
 		this.score(word);
-		round.accuracy += 1;
+		this.round.accuracy += 1;
 		this.updateUI();
 	};
 
 	// incorrect keypress
 	badKey() {
-		bubbles[cur].bad++;
-		round.accuracy -= (bubbles[cur].bad / bubbles[cur].length);
+		this.bubbles[this.cur].bad++;
+		this.round.accuracy -= (this.bubbles[this.cur].bad / this.bubbles[this.cur].length);
 	};
 
 	// a word's just been deleted off the screen
 	deleted(popped) {
 		if(!popped) { // missed a word
-			round.accuracy -= 1;
+			this.round.accuracy -= 1;
 			this.elapsed();
 		}
 
@@ -448,24 +443,24 @@ class Game {
 			e.preventDefault();
 		}
 
-		if(!cur || !bubbles[cur] || createjs.Ticker.getPaused()) return;
+		if(!this.cur || !this.bubbles[this.cur] || createjs.Ticker.getPaused()) return;
 
 		var c = String.fromCharCode(e.keyCode).toUpperCase(),
-			p = bubbles[cur].pointer;
+			p = this.bubbles[this.cur].pointer;
 
-		if(!bubbles[cur][p]) {
+		if(!this.bubbles[this.cur][p]) {
 			return;
 		}
-		if( bubbles[cur][p].char == c) {	// correct keypress
-			bubbles[cur][p].speed = 30;
-			bubbles[cur].pointer++;
+		if( this.bubbles[this.cur][p].char == c) {	// correct keypress
+			this.bubbles[this.cur][p].speed = 30;
+			this.bubbles[this.cur].pointer++;
 			this.popOne(c);
 		} else { // incorrect keypress
 			this.badKey();
 		}
 
-		if(bubbles[cur].pointer >= bubbles[cur].length) {
-			this.popped(cur);
+		if(this.bubbles[this.cur].pointer >= this.bubbles[this.cur].length) {
+			this.popped(this.cur);
 		}
 	}
 
