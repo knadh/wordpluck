941ea84e755c0d667f9db3487ed6090be161550b
HARSHITH NAIDU <322103310062@gvpce.ac.in>
2023-05-23 11:02:27 +0530

    inc

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ class Game {
 		this.RADIUS = 30
 		this.BORDER = 4
 		this.FONT_SIZE = 36
-		this.FPS = 60
+		this.FPS = 160
 		this.SCORE_LETTER = 100
 		this.WORDS_PER_LEVEL = 3
 		this.round = {
